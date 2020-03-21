e6ded89cd0089b437efd9c8d6ad7dba5958ecd5d
Thomas Pelletier <pelletier.thomas@gmail.com>
2020-03-21 09:09:00 -0400

    POC

---
+++ cmd/multi-init/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"sync"
+)
+
+func run(i int, ctx context.Context, deadChan chan<- int, args ...string) {
+	prefix := fmt.Sprintf("[%d]:", i)
+	log.Println(prefix, "starting", args)
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	err := cmd.Run()
+	if err == nil {
+		log.Println(prefix, "terminated gracefully")
+	} else {
+		log.Println(prefix, "terminated with error:", err)
+	}
+	deadChan<-i
+}
+
+func main() {
+	programsArgs := [][]string{
+		{"/usr/bin/sleep", "5"},
+		{"/bin/bash", "-c", "while true; do echo 'still alive'; sleep 1; done"}, 
+	}
+
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	deadChan := make(chan int, len(programsArgs))
+
+	for i, args := range programsArgs {
+		wg.Add(1)
+		go func(i int, args []string) {
+			defer wg.Done()
+			run(i, ctx, deadChan, args...)
+		}(i, args)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+	exitCode := 0
+	select {
+	case sig := <-sigChan:
+		switch sig {
+		case os.Interrupt:
+			log.Println("- SIGINT -")
+		case os.Kill:
+			log.Println("- SIGKILL -")
+		}
+	case i := <- deadChan:
+		log.Printf("process [%d] died. stopping everything", i)
+		exitCode = 1
+	}
+	cancel()
+	wg.Wait()
+	close(deadChan)
+	os.Exit(exitCode)
+}
